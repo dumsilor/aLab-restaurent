81d1418fcee899da60f040972b898820446765e0
Mosiur Rahman <connect.mosiur@gmail.com>
2024-08-31 22:29:00 +0600

    added landing-page-content image and text, styling done, responsiveness pending

---
+++ src/app/business-feature/business-feature.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-business-feature',
+  standalone: true,
+  imports: [],
+  templateUrl: './business-feature.component.html',
+  styleUrl: './business-feature.component.scss'
+})
+export class BusinessFeatureComponent {
+
+}
