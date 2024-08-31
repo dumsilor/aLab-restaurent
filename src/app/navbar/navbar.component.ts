57c0c0470b289bd4bb6e27b707c7ec720a049c82
Mosiur Rahman <connect.mosiur@gmail.com>
2024-08-31 16:39:09 +0600

    added nav and user entry, navbar Completed large screen

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+
 import { LogoComponent } from './logo/logo.component';
+import { NavComponent } from './nav/nav.component';
+import { EntryComponent } from './entry/entry.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [LogoComponent],
+  imports: [LogoComponent, NavComponent, EntryComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
